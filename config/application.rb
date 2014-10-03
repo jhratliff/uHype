933c98c99a5b2e240446fc0f2849eaa6e4dd449d
jhratliff <jamesr@gmail.com>
2014-10-03 16:19:20 -0700

    Adds heroku-specific switches for precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ module UHype
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
