47437fe27460ad104114d94ee739babf866ef6e4
jhratliff <jamesr@gmail.com>
2014-10-03 22:27:56 -0700

    Removes endpoint params for AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,9 +11,9 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
         :provider => 'AWS',
         :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-        :region => ENV['S3_REGION'],
-        :endpoint => ENV['S3_ENDPOINT']
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        # :region => ENV['S3_REGION'],
+        # :endpoint => ENV['S3_ENDPOINT']
     }
 
     # To let CarrierWave work on heroku
