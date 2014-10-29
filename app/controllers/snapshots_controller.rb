82380dca54b2912fe5c7b603adb18f286c89b49c
jhratliff <jamesr@gmail.com>
2014-10-29 09:32:48 -0700

    Adds base64 image handler for the snapshot model

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -19,13 +19,39 @@ class SnapshotsController < ApplicationController
   end
 
   def create
+
     @snapshot = Snapshot.new(snapshot_params)
+
+    # presupposes this format:
+    #snapshot {:user_id => "1", :like_count => 1, :unlike_count => 1, :flag_count => 1, etc., :snapshot_path {:file => "base64 encoded awesomeness", :original_filename => "my file name", :filename => "my file name"}}
+
+    #check if file is within picture_path
+    if params[:snapshot][:snapshot_path]["file"]
+      snapshot_path_params = params[:snapshot][:snapshot_path]
+
+      #create a new tempfile named fileupload
+      tempfile = Tempfile.new("fileupload")
+      tempfile.binmode
+
+      #get the file and decode it with base64 then write it to the tempfile
+      tempfile.write(Base64.decode64(snapshot_path_params["file"]))
+
+      #create a new uploaded file
+      uploaded_file = ActionDispatch::Http::UploadedFile.new(:tempfile => tempfile, :filename => snapshot_path_params["filename"], :original_filename => snapshot_path_params["original_filename"])
+
+      #replace picture_path with the new uploaded file
+      params[:snapshot][:snapshot_path] = uploaded_file
+    end
+
+
     @snapshot.save
     respond_with(@snapshot)
   end
 
   def update
     @snapshot.update(snapshot_params)
+    @snapshot.photo = params[:file]
+    @snapshot.save
     respond_with(@snapshot)
   end
 
@@ -35,11 +61,11 @@ class SnapshotsController < ApplicationController
   end
 
   private
-    def set_snapshot
-      @snapshot = Snapshot.find(params[:id])
-    end
+  def set_snapshot
+    @snapshot = Snapshot.find(params[:id])
+  end
 
-    def snapshot_params
-      params.require(:snapshot).permit(:URL, :like_count, :unlike_count, :flag_count, :user_id)
-    end
+  def snapshot_params
+    params.require(:snapshot).permit(:URL, :like_count, :unlike_count, :flag_count, :user_id, :photo, :photo_cache)
+  end
 end
