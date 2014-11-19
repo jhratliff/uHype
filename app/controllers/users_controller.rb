5afa24fd65790e06d5e03c2763d758323abd1825
jhratliff <jamesr@gmail.com>
2014-11-18 22:49:32 -0800

    Adds requested status for private follow requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,49 +45,51 @@ class UsersController < ApplicationController
   def follow
     @user = current_user
     if params[:user_id]
-      @user.friends << User.find(params[:user_id])
+      if current_user.is_private
+        @requestor = User.find(params[:user_id])
+        @following = @user.followeds.where(:user => @requestor)
+        @following.status = "requested"
+        @following.save
+      else
+        @user.friends << User.find(params[:user_id])
+      end
     end
-  end
-
-  def unfollow
-    @user = current_user
-    if params[:user_id]
-      @user.friends.destroy(User.find(params[:user_id]))
-    end
-  end
-
-  def friends
-    @user = current_user
-    respond_with(@user)
-  end
-
-
-
-
-
-
-  def feed
-    # returns the user's feed
-    # feed items are the most recent items from all the schools I follow (including my own school)
-    # feed items include:
-    # User's Full Record (including High School name)
-    # Post Date
-    # likes_count
-    # unlikes_count
-    #
-    # AND
-    #
-    # the same data for all the individuals I follow (their posts)
-
-
-
 
   end
+end
 
-  private
-
-  def secure_params
-    params.require(:user).permit(:role, :first_name,:last_name,:dob, :class_of, :school_id, :is_private)
+def unfollow
+  @user = current_user
+  if params[:user_id]
+    @user.friends.destroy(User.find(params[:user_id]))
   end
+end
+
+def friends
+  @user = current_user
+  respond_with(@user)
+end
+
+
+def feed
+  # returns the user's feed
+  # feed items are the most recent items from all the schools I follow (including my own school)
+  # feed items include:
+  # User's Full Record (including High School name)
+  # Post Date
+  # likes_count
+  # unlikes_count
+  #
+  # AND
+  #
+  # the same data for all the individuals I follow (their posts)
+
 
 end
+
+private
+
+def secure_params
+  params.require(:user).permit(:role, :first_name, :last_name, :dob, :class_of, :school_id, :is_private)
+end
+
