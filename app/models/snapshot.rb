4bfd456d8b9d723823582bfe0b6816c44d145f2b
jhratliff <jamesr@gmail.com>
2015-01-31 16:30:23 -0800

    Adds status for whether I have liked/unliked/flagged the snapshot

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -2,7 +2,16 @@ class Snapshot < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
 
   belongs_to :user
-  has_many :snapshot_flags
+
   has_many :snapshot_likes
+  has_many :snapshot_likers, :through => :snapshot_likes, :source => :user
+
+
   has_many :snapshot_unlikes
+  has_many :snapshot_unlikers, :through => :snapshot_unlikes, :source => :user
+
+  has_many :snapshot_flags
+  has_many :snapshot_flaggers, :through => :snapshot_flags, :source => :user
+
+
 end
--- app/views/snapshots/show.json.jbuilder
+++ app/views/snapshots/show.json.jbuilder
@@ -1,5 +1,13 @@
 json.extract! @snapshot, :id, :URL, :like_count, :unlike_count, :flag_count, :user_id, :created_at, :updated_at
 
+
+json.liked @snapshot.snapshot_likers.include?(@user)
+json.unliked @snapshot.snapshot_unlikers.include?(@user)
+json.flagged @snapshot.snapshot_flaggers.include?(@user)
+
+
+
+
 json.photo @snapshot.photo.url
 json.photo_large @snapshot.photo.large.url
 json.photo_medium @snapshot.photo.medium.url
