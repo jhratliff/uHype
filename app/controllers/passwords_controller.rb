1d587df10ea488892e5b4068e17542ae72dbb12d
jhratliff <jamesr@gmail.com>
2015-04-19 19:28:16 -0700

    Adds a password controller

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,29 @@
+class PasswordsController < ApplicationController
+  respond_to :json
+
+  def reset_password
+
+    reason = 'bar reason'
+    status = 'foo status'
+    email_address = params[:lost_password_email]
+
+    puts ("@@@@@@@@@@@ lost password email is: #{params[:lost_password_email]} ")
+
+    if(!email_address.nil?)
+      puts ("@@@@@@@@@@@ password is not nil ")
+
+      status = 'Success'
+      reason = "Check your email (#{email_address}) for the updated password"
+
+    else
+      puts ("@@@@@@@@@@@ password is nil ")
+      status = 'Error'
+      reason = 'Empty email address was sent'
+    end
+
+    @response = {:status => status, :reason => reason}
+
+    respond_with(@response)
+
+  end
+end
